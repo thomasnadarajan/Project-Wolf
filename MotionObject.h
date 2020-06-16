c7a2ad11c275e176c65aa816ad82a0ce861fff99
Tom <thomas.nadarajan@gmail.com>
2020-06-16 18:49:14 +1000

    used Stanford example

---
+++ MotionObject.h
@@ -0,0 +1,5 @@
+#include "CollisionObject.h"
+
+class MotionObject {
+    
+};
---
+++ temp.c
@@ -0,0 +1,11 @@
+struct node {
+    struct node * next;
+    struct node * prev;
+    int * value;
+};
+
+void add_value(struct node ** valuep, int *v) {
+
+    struct node ** v = malloc (sizeof **v);
+    struct node *val = malloc(sizeof *val);
+}
