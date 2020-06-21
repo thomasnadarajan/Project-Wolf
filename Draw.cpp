2d61f033dcf7e627baa7ea17c36bcac0f89cc8a4
Daniel Stoilescu <46283690+mrbunton@users.noreply.github.com>
2020-06-22 00:16:43 +1000

    coordinate system, bug fix
    -fixed the draw method so it draws rectangles instead of triangles
    -added coordinate system conversion that used integers that will be converted into double value that glut will read.

--- Draw.cpp
+++ Draw.cpp
@@ -1,13 +1,18 @@
 #include "Draw.h"
 
+double coord_to_screen(int x) {
+    return x/100.0;
+}
+
 void draw_vertices(Object& obj) {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
     glClear(GL_COLOR_BUFFER_BIT);       
     glBegin(GL_QUADS);
-    glVertex2f(obj.hitbox.main_hitbox.x - obj.hitbox.main_hitbox.width, obj.hitbox.main_hitbox.y - obj.hitbox.main_hitbox.height);
-    glVertex2f(obj.hitbox.main_hitbox.x + obj.hitbox.main_hitbox.width, obj.hitbox.main_hitbox.y - obj.hitbox.main_hitbox.height);
-    glVertex2f(obj.hitbox.main_hitbox.x - obj.hitbox.main_hitbox.width, obj.hitbox.main_hitbox.y + obj.hitbox.main_hitbox.height);
-    glVertex2f(obj.hitbox.main_hitbox.x + obj.hitbox.main_hitbox.width, obj.hitbox.main_hitbox.y + obj.hitbox.main_hitbox.height);
+    glVertex2f(coord_to_screen(obj.hitbox.main_hitbox.x), coord_to_screen(obj.hitbox.main_hitbox.y));
+    glVertex2f(coord_to_screen(obj.hitbox.main_hitbox.x + obj.hitbox.main_hitbox.width), coord_to_screen(obj.hitbox.main_hitbox.y ));
+    glVertex2f(coord_to_screen(obj.hitbox.main_hitbox.x + obj.hitbox.main_hitbox.width), coord_to_screen(obj.hitbox.main_hitbox.y + obj.hitbox.main_hitbox.height));
+    glVertex2f(coord_to_screen(obj.hitbox.main_hitbox.x), coord_to_screen(obj.hitbox.main_hitbox.y + obj.hitbox.main_hitbox.height));
+    
     glEnd();
     glFlush();
 }
