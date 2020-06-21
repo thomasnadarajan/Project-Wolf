2a86d867d1cc7b2016b33dbc2cf072877fe7fbcf
Tom <thomas.nadarajan@gmail.com>
2020-06-22 00:34:40 +1000

    update 00:34

--- Draw.h
+++ Draw.h
@@ -3,9 +3,6 @@
 coordinates go -100,100 on the screen
 */
 double coord_to_screen(int a);
-<<<<<<< HEAD
-=======
 
 void draw_vertices(Object& obj);
->>>>>>> 2d61f033dcf7e627baa7ea17c36bcac0f89cc8a4
 
